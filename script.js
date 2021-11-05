f8ee3e6bf1d1e189130fad5288d4633ee19952d2
TuranKh <turan.khidilov@gmail.com>
2021-11-05 11:08:02 +0400

    Sneakers

---
+++ script.js
@@ -0,0 +1,11 @@
+// function setColor() {
+//     let last = 
+//     var stars = document.getElementsByClassName('star')
+//     console.log(stars)
+
+//     let initial = 0;
+//     while(initial<last) {
+//         stars[initial].style.filter = "contrast(1)";
+//         initial ++;    
+//     }
+// }
